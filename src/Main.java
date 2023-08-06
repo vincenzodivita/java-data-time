4341a09663da46c4f5f2e039e3ab6d0699829ad2
vincenzodivita <divitavincenzo@icloud.com>
2023-08-06 16:46:38 +0200

    Fix print result

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,7 @@ public class Main {
         DateTimeFormatter formattedTime = DateTimeFormatter.ofPattern("HH:mm");
         DateTimeFormatter formattedDay = DateTimeFormatter.ofPattern("EEEE");
 
-        System.out.println(dateAndTime.format(formattedDay));
-        System.out.println(dateAndTime.format(formattedDate));
+        System.out.println(dateAndTime.format(formattedDay) + " " + dateAndTime.format(formattedDate));
         System.out.println(dateAndTime.format(formattedTime));
     }
 }
