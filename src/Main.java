91b918d0da33133725a686597c091ed6dc73a7ad
vincenzodivita <divitavincenzo@icloud.com>
2023-08-06 16:31:47 +0200

    Print formatted date

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
 
         LocalDate date = LocalDate.now(); 
 
-        System.out.println(date);
+        DateTimeFormatter formattedDate = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println(date.format(formattedDate));
         
     }
 }
