97e395d0a0c88a25c8eef5bb284ee194482daca1
vincenzodivita <divitavincenzo@icloud.com>
2023-08-06 16:15:56 +0200

    Print date

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        LocalDate date = LocalDate.now(); 
+
+        System.out.println(date);
+        
     }
 }
